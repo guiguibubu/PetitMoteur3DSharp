5ba2a6998a3b693fb5b22d81d5121d45ad6edcbd
guiguibubu <guibu44@hotmail.fr>
2024-09-27 20:08:06 +0200

    3D Device init steps in subfunctions

--- PetitMoteur3D/DeviceD3D11.cs
+++ PetitMoteur3D/DeviceD3D11.cs
@@ -45,79 +45,10 @@ namespace PetitMoteur3D
 
             _compiler = D3DCompiler.GetApi();
 
-            uint createDeviceFlags = 0;
-#if DEBUG
-            createDeviceFlags |= (uint)CreateDeviceFlag.Debug;
-#endif
+            InitDevice(forceDxvk);
 
-            // Create our D3D11 logical device.
-            D3D11 d3d11 = D3D11.GetApi(_window, forceDxvk);
-
-            D3DFeatureLevel[] featureLevels = {
-                D3DFeatureLevel.Level111,
-                D3DFeatureLevel.Level110
-            };
-            SilkMarshal.ThrowHResult
-            (
-                d3d11.CreateDevice
-                (
-                    default(ComPtr<IDXGIAdapter>),
-                    D3DDriverType.Hardware,
-                    default,
-                    createDeviceFlags,
-                    in featureLevels[0],
-                    2,
-                    D3D11.SdkVersion,
-                    ref _device,
-                    null,
-                    ref _deviceContext
-                )
-            );
-            d3d11.Dispose();
-
-            //This is not supported under DXVK 
-            //TODO: PR a stub into DXVK for this maybe?
-            if (OperatingSystem.IsWindows())
-            {
-                // Log debug messages for this device (given that we've enabled the debug flag). Don't do this in release code!
-                _device.SetInfoQueueCallback(msg => Console.WriteLine(SilkMarshal.PtrToString((nint)msg.PDescription)));
-            }
-
-            // Create our swapchain description.
-            SwapChainDesc1 swapChainDesc = new()
-            {
-                Width = (uint)window.Size.X,
-                Height = (uint)window.Size.Y,
-                BufferCount = 1,
-                Format = Format.FormatB8G8R8A8Unorm,
-                BufferUsage = DXGI.UsageRenderTargetOutput,
-                SampleDesc = new SampleDesc(1, 0),
-                Flags = (uint)SwapChainFlag.AllowModeSwitch
-            };
-
-            SwapChainFullscreenDesc swapChainFullscreenDesc = new()
-            {
-                Windowed = true
-            };
-
-            // Create our DXGI factory to allow us to create a swapchain. 
-            DXGI dxgi = DXGI.GetApi(_window, forceDxvk);
-            ComPtr<IDXGIFactory2> factory = dxgi.CreateDXGIFactory<IDXGIFactory2>();
-            dxgi.Dispose();
-
-            // Create the swapchain.
-            SilkMarshal.ThrowHResult
-            (
-                factory.CreateSwapChainForHwnd
-                (
-                    _device,
-                    _window.Native!.DXHandle!.Value,
-                    in swapChainDesc,
-                    in swapChainFullscreenDesc,
-                    ref Unsafe.NullRef<IDXGIOutput>(),
-                    ref _swapchain
-                )
-            );
+            // Initialisatio de la swapchain
+            InitSwapChain(window, forceDxvk);
 
             // Create « render target view » 
             // Obtain the framebuffer for the swapchain's backbuffer.
@@ -129,6 +60,7 @@ namespace PetitMoteur3D
             // Tell the output merger about our render target view.
             _deviceContext.OMSetRenderTargets(1, ref _renderTargetView, _depthStencilView);
             _deviceContext.ClearRenderTargetView(_renderTargetView, ref _backgroundColour[0]);
+            _deviceContext.ClearDepthStencilView(_depthStencilView, (uint)ClearFlag.Depth, 1.0f, 0);
 
             // Set the rasterizer state with the current viewport.
             Viewport viewport = new(0, 0, _window.FramebufferSize.X, _window.FramebufferSize.Y, 0, 1);
@@ -220,6 +152,87 @@ namespace PetitMoteur3D
             {
                 System.Console.WriteLine("EndFrameCapture fail to capture");
             }
+        }
+
+        private unsafe void InitDevice(bool forceDxvk)
+        {
+            uint createDeviceFlags = 0;
+#if DEBUG
+            createDeviceFlags |= (uint)CreateDeviceFlag.Debug;
+#endif
+
+            // Create our D3D11 logical device.
+            D3D11 d3d11 = D3D11.GetApi(_window, forceDxvk);
+
+            D3DFeatureLevel[] featureLevels = {
+                D3DFeatureLevel.Level111,
+                D3DFeatureLevel.Level110
+            };
+            SilkMarshal.ThrowHResult
+            (
+                d3d11.CreateDevice
+                (
+                    default(ComPtr<IDXGIAdapter>),
+                    D3DDriverType.Hardware,
+                    default,
+                    createDeviceFlags,
+                    in featureLevels[0],
+                    2,
+                    D3D11.SdkVersion,
+                    ref _device,
+                    null,
+                    ref _deviceContext
+                )
+            );
+            d3d11.Dispose();
+
+            //This is not supported under DXVK 
+            //TODO: PR a stub into DXVK for this maybe?
+            if (OperatingSystem.IsWindows())
+            {
+                // Log debug messages for this device (given that we've enabled the debug flag). Don't do this in release code!
+                _device.SetInfoQueueCallback(msg => Console.WriteLine(SilkMarshal.PtrToString((nint)msg.PDescription)));
+            }
+        }
+
+        private unsafe void InitSwapChain(IWindow window, bool forceDxvk)
+        {
+            // Create our swapchain description.
+            SwapChainDesc1 swapChainDesc = new()
+            {
+                Width = (uint)window.Size.X,
+                Height = (uint)window.Size.Y,
+                BufferCount = 1,
+                Format = Format.FormatB8G8R8A8Unorm,
+                BufferUsage = DXGI.UsageRenderTargetOutput,
+                SampleDesc = new SampleDesc(1, 0),
+                Flags = (uint)SwapChainFlag.AllowModeSwitch
+            };
+
+            SwapChainFullscreenDesc swapChainFullscreenDesc = new()
+            {
+                Windowed = true
+            };
+
+            // Create our DXGI factory to allow us to create a swapchain. 
+            DXGI dxgi = DXGI.GetApi(_window, forceDxvk);
+            ComPtr<IDXGIFactory2> factory = dxgi.CreateDXGIFactory<IDXGIFactory2>();
+            dxgi.Dispose();
+
+            // Create the swapchain.
+            SilkMarshal.ThrowHResult
+            (
+                factory.CreateSwapChainForHwnd
+                (
+                    _device,
+                    _window.Native!.DXHandle!.Value,
+                    in swapChainDesc,
+                    in swapChainFullscreenDesc,
+                    ref Unsafe.NullRef<IDXGIOutput>(),
+                    ref _swapchain
+                )
+            );
+
         }
 
         private unsafe void InitRenderTargetView()
